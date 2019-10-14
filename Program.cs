1f9a95dc4b046150502b3a386350069a5df6a7b0
Rafael Tongson <rtongson@peppymiint.local>
2019-10-14 13:46:00 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fundamentalsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for(int j = 1; j <= 100; j++)
+            {
+                if((j % 3 == 0 || j % 5 == 0) && j % 15 != 0) 
+                {
+                    Console.WriteLine(j);
+                }
+            }
+        }
+    }
+}
